388e14622609b6b3b4f4c172644b39053e3a1c3c
AlexFromNorth_desc <jacklex@bk.ru>
2024-03-12 04:00:34 +0300

    added default styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import "firebase/firestore";
 import Main from "./pages/main/Main.tsx";
+import Header from "./components/header/Header.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Main />
-    </div>
+      <Footer />
+    </>
   );
 }
 
