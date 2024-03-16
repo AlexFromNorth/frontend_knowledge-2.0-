d70be386b5310bccec6eedf06dab4ff345067b40
AlexFromNorth_desc <jacklex@bk.ru>
2024-03-16 23:36:44 +0300

    component and buisness logic for auth

---
+++ src/components/auth/AuthDetails.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+const AuthDetails = () => {
+  const [authUser, setAuthUser] = useState(null);
+
+  //   useEffect(() => {
+  //     const listen = onAuthStateChanged();
+  //   });
+
+  return <div>AuthDetails</div>;
+};
+
+export default AuthDetails;
