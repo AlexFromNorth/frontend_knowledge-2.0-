ea8858cdf7ce61ddb210d24e814cd54e063e86b6
AlexFromNorth_desc <jacklex@bk.ru>
2024-04-07 17:04:19 +0300

    created comments component

--- src/components/collectionContent/filter.ts
+++ src/components/collectionContent/filter.ts
@@ -1,3 +1,7 @@
 export const filter = (arr, document) => {
   return arr.filter((el) => el.id == document);
 };
+
+export function getKeyByValueNegate(obj, value) {
+  return Object.keys(obj).filter((key) => key != value);
+}
--- src/components/comments/Comment.tsx
+++ src/components/comments/Comment.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { RootState } from "../../redux/store";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { filter, getKeyByValueNegate } from "../collectionContent/filter.ts";
 
 const Comment = () => {
   return <div>Comment</div>;
